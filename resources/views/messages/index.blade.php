8b7828a6c073d8a87163e90f9a189696477c68d3
murata.murata <mkmktest1007@gmail.com>
2020-04-30 09:09:30 +0000

    add seeder

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -24,6 +24,7 @@
             </tbody>
         </table>
     @endif
+    {{ $messages->links('pagination::bootstrap-4') }}
     {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
     
 
